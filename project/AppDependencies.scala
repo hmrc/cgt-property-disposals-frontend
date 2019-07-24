c1a3511abdda33d7f048ac0bd01cf352951fbe1d
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2019-07-24 10:20:14 +0100

    cpdp-431: used GDS page title template

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,8 @@ object AppDependencies {
 
     "uk.gov.hmrc"             %% "govuk-template"           % "5.36.0-play-26",
     "uk.gov.hmrc"             %% "play-ui"                  % "7.40.0-play-26",
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "0.42.0"
+    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "0.42.0",
+    "org.typelevel"           %% "cats-core"                % "1.1.0"
   )
 
   val test = Seq(
