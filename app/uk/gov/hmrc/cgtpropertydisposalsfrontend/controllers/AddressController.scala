25e39ea4aa30896bec92496dda54c4e8825a4d03
Andy <AndyHWChung@users.noreply.github.com>
2019-08-06 09:45:32 +0100

    postcode submit

--- app/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/AddressController.scala
+++ app/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/AddressController.scala
@@ -19,24 +19,36 @@ package uk.gov.hmrc.cgtpropertydisposalsfrontend.controllers
 import com.google.inject.Inject
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.config.ViewConfig
-import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.Address
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.{Address, Postcode}
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.Address.UkAddress
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.util.Logging
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.views
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
 
 class AddressController @Inject() (
-    cc: MessagesControllerComponents,
-    enterPostcodePage: views.html.subscription.enter_postcode,
-    selectAddressPage: views.html.subscription.select_address
-)(implicit viewConfig: ViewConfig) extends FrontendController(cc) {
+                                    cc: MessagesControllerComponents,
+                                    enterPostcodePage: views.html.subscription.enter_postcode,
+                                    selectAddressPage: views.html.subscription.select_address
+                                  )(implicit viewConfig: ViewConfig) extends FrontendController(cc) with Logging {
 
   def enterPostcode(): Action[AnyContent] = Action { implicit request =>
-    Ok(enterPostcodePage())
+    Ok(enterPostcodePage(Postcode.form))
   }
 
+  def enterPostcodeSubmit(): Action[AnyContent] = Action { implicit request =>
+    Postcode.form.bindFromRequest().fold(
+      formWithErrors => BadRequest(enterPostcodePage(formWithErrors)),
+      { postcode =>
+        logger.info(s"Got postcode $postcode")
+        SeeOther(routes.AddressController.selectAddress().url)
+      }
+    )
+  }
+
+
   def selectAddress(): Action[AnyContent] = Action { implicit request =>
-      def address(i: Int) =
-        UkAddress(s"$i the street", Some("The Town"), Some("The County"), None, "postcode")
+    def address(i: Int) =
+      UkAddress(s"$i the street", Some("The Town"), Some("The County"), None, "postcode")
 
     val addresses = (0 to 100).map(address).toList
     Ok(selectAddressPage(addresses))
