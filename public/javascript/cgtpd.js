90bac0cbb9d0094e049b00ee851312e5e53f6dd5
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2019-09-27 16:45:38 +0100

    cpdp-600:  fix for autcomplete to reset select element when input empty
    handled non matching strings
    removed UK/GB from autocomplete graph

--- public/javascript/cgtpd.js
+++ public/javascript/cgtpd.js
@@ -1,8 +1,34 @@
-$('.error-summary').focus();
+
+var errorSummary = document.querySelector('.error-summary');
+if (errorSummary) {
+  errorSummary.focus()
+}
 var countryEl = document.querySelector("#countryCode");
 if(countryEl) {
   openregisterLocationPicker({
     selectElement: countryEl,
-    url: '/cgt-property-disposals/assets/location-autocomplete-graph.json'
+    url: '/cgt-property-disposals/assets/location-autocomplete-graph.json',
+    defaultValue: ''
   });
+  // patch to ensure clearing the pseudo input also clears the select element
+  // this is an active issue on accessible-autocomplete
+  // https://github.com/alphagov/accessible-autocomplete/issues/260
+  // when it is resolved we can remove this code
+  var wrapper = document.querySelector('.country-code-wrapper');
+  function resetSelectIfEmpty (e) {
+    if (e.target.id === 'countryCode') {
+      var val = e.target.value.trim();
+      var countrySelect = document.querySelector("#countryCode-select");
+      if (countrySelect) {
+        var countriesArray = Array.prototype.slice.call(countrySelect.options);
+        var matches = countriesArray.filter(function (o) {
+          return o.text === val
+        });
+        if (!matches.length) {
+          countrySelect.value = ''
+        }
+      }
+    }
+  }
+  wrapper.addEventListener('change', resetSelectIfEmpty);
 }
