5fc43a054a524cd67e098263f46285026bacfd7e
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2019-10-22 11:09:13 +0100

    cpdp-477: iv error pages (#67)

--- app/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/IvController.scala
+++ app/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/IvController.scala
@@ -18,15 +18,20 @@ package uk.gov.hmrc.cgtpropertydisposalsfrontend.controllers
 
 import java.util.UUID
 
+import cats.instances.future._
 import cats.syntax.eq._
 import com.google.inject.{Inject, Singleton}
+import play.api.Configuration
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import uk.gov.hmrc.cgtpropertydisposalsfrontend.config.ErrorHandler
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.config.{ErrorHandler, ViewConfig}
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.controllers.actions.{AuthenticatedAction, SessionDataAction, WithAuthAndSessionDataAction}
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.SessionData
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.iv.IvErrorStatus
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.repos.SessionStore
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.services.IvService
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.util.Logging._
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.util.{Logging, toFuture}
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.views
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
 
 import scala.concurrent.ExecutionContext
@@ -36,15 +41,26 @@ class IvController @Inject()(
   sessionStore: SessionStore,
   val authenticatedAction: AuthenticatedAction,
   val sessionDataAction: SessionDataAction,
-  errorHandler: ErrorHandler,
-  cc: MessagesControllerComponents
-)(implicit ec: ExecutionContext)
+  val errorHandler: ErrorHandler,
+  val config: Configuration,
+  ivService: IvService,
+  cc: MessagesControllerComponents,
+  failedIvPage: views.html.iv.failed_iv,
+  failedMatchingPage: views.html.iv.failed_matching,
+  insufficientEvidencePage: views.html.iv.insufficient_evidence,
+  lockedOutPage: views.html.iv.locked_out,
+  preconditionFailedPage: views.html.iv.precondition_failed,
+  technicalIssuesPage: views.html.iv.technical_iv_issues,
+  timeoutPage: views.html.iv.time_out,
+  userAbortedPage: views.html.iv.user_aborted
+)(implicit ec: ExecutionContext, viewConfig: ViewConfig)
     extends FrontendController(cc)
     with WithAuthAndSessionDataAction
     with Logging
-    with SessionUpdates {
+    with SessionUpdates
+    with IvBehaviour {
 
-  def ivSuccess(): Action[AnyContent] = authenticatedActionWithSessionData.async { implicit request =>
+  def ivSuccessCallback(): Action[AnyContent] = authenticatedActionWithSessionData.async { implicit request =>
     if (request.sessionData.forall(_ === SessionData.empty)) {
       SeeOther(routes.StartController.start().url)
     } else {
@@ -59,9 +75,64 @@ class IvController @Inject()(
     }
   }
 
-  def ivFailure(journeyId: UUID): Action[AnyContent] = Action { implicit request =>
-    logger.warn(s"IV journey failed: ${journeyId.toString}")
-    InternalServerError(errorHandler.internalServerErrorTemplate)
+  def retry():  Action[AnyContent] = authenticatedActionWithSessionData.async {
+    implicit request => redirectToIv
+  }
+
+  def ivFailureCallback(journeyId: UUID): Action[AnyContent] = authenticatedActionWithSessionData.async {
+    implicit request =>
+      ivService
+        .getFailedJourneyStatus(journeyId)
+        .fold(
+          { e =>
+            logger.warn("Could not check IV journey error status", e)
+            Redirect(routes.IvController.getTechnicalIssue())
+          }, {
+            case IvErrorStatus.Incomplete           => Redirect(routes.IvController.getTechnicalIssue())
+            case IvErrorStatus.FailedMatching       => Redirect(routes.IvController.getFailedMatching())
+            case IvErrorStatus.FailedIV             => Redirect(routes.IvController.getFailedIV())
+            case IvErrorStatus.InsufficientEvidence => Redirect(routes.IvController.getInsufficientEvidence())
+            case IvErrorStatus.LockedOut            => Redirect(routes.IvController.getLockedOut())
+            case IvErrorStatus.UserAborted          => Redirect(routes.IvController.getUserAborted())
+            case IvErrorStatus.Timeout              => Redirect(routes.IvController.getTimedOut())
+            case IvErrorStatus.TechnicalIssue       => Redirect(routes.IvController.getTechnicalIssue())
+            case IvErrorStatus.PreconditionFailed   => Redirect(routes.IvController.getPreconditionFailed())
+            case IvErrorStatus.Unknown(value) =>
+              logger.warn(s"Received unknown error response status from IV: $value")
+              Redirect(routes.IvController.getTechnicalIssue())
+          }
+        )
+  }
+
+  def getFailedMatching: Action[AnyContent] = authenticatedActionWithSessionData { implicit r ⇒
+    Ok(failedMatchingPage())
+  }
+
+  def getFailedIV: Action[AnyContent] = authenticatedActionWithSessionData { implicit r ⇒
+    Ok(failedIvPage())
+  }
+
+  def getInsufficientEvidence: Action[AnyContent] = authenticatedActionWithSessionData { implicit r ⇒
+    Ok(insufficientEvidencePage())
+  }
+
+  def getLockedOut: Action[AnyContent] = authenticatedActionWithSessionData { implicit r ⇒
+    Ok(lockedOutPage())
+  }
+
+  def getUserAborted: Action[AnyContent] = authenticatedActionWithSessionData { implicit r ⇒
+    Ok(userAbortedPage())
+  }
+
+  def getTimedOut: Action[AnyContent] = authenticatedActionWithSessionData { implicit r ⇒
+    Ok(timeoutPage())
+  }
+
+  def getTechnicalIssue: Action[AnyContent] = authenticatedActionWithSessionData { implicit r ⇒
+    Ok(technicalIssuesPage())
+  }
+  def getPreconditionFailed: Action[AnyContent] = authenticatedActionWithSessionData { implicit r ⇒
+    Ok(preconditionFailedPage())
   }
 
 }
