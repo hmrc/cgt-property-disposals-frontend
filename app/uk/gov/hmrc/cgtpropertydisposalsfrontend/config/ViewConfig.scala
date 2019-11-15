a211c88b671ddfba5d99b805d763e4b996127471
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2019-11-15 14:10:07 +0000

    cpdp-968: new exit survey link on relevant pages

--- app/uk/gov/hmrc/cgtpropertydisposalsfrontend/config/ViewConfig.scala
+++ app/uk/gov/hmrc/cgtpropertydisposalsfrontend/config/ViewConfig.scala
@@ -54,4 +54,5 @@ class ViewConfig @Inject()(servicesConfig: ServicesConfig) {
   val fileCorporationTaxUrl: String = getString("external-url.file-corporation-tax")
   val trustHelpUrl: String = getString("external-url.trust-help")
   val tellHmrcChangeDetails: String = getString("external-url.tell-hmrc-change-details")
+  val exitSurveyUrl: String = getString("exit-survey.uri")
 }
