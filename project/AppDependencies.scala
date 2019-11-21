7731e72a1779cd9cd0b46fdd534d550fa4461e1c
Andy <AndyHWChung@users.noreply.github.com>
2019-11-21 16:03:35 +0000

    cpdp-993: removed sautr checksum chck

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,8 +13,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "mongo-caching"            % "6.6.0-play-26",
     "org.typelevel"           %% "cats-core"                % "1.6.1",
     "com.github.kxbmap"       %% "configs"                  % "0.4.4",
-    "org.julienrf"            %% "play-json-derived-codecs" % "3.3",
-    "uk.gov.hmrc"             %% "domain"                   % "5.6.0-play-26"
+    "org.julienrf"            %% "play-json-derived-codecs" % "3.3"
   )
 
   val test = Seq(
