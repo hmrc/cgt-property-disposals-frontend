d0c27ed647759c7c9d1781629f19d022b1ab8dae
Andy <AndyHWChung@users.noreply.github.com>
2019-12-16 11:30:34 +0000

    exlucded template and assets from filter

--- app/uk/gov/hmrc/cgtpropertydisposalsfrontend/filters/EmailWhitelistingFilter.scala
+++ app/uk/gov/hmrc/cgtpropertydisposalsfrontend/filters/EmailWhitelistingFilter.scala
@@ -17,6 +17,10 @@
 package uk.gov.hmrc.cgtpropertydisposalsfrontend.filters
 
 import akka.stream.Materializer
+import cats.Eq
+import cats.instances.boolean._
+import cats.instances.string._
+import cats.syntax.eq._
 import com.google.inject.Inject
 import play.api.Configuration
 import play.api.mvc.Results.Redirect
@@ -41,6 +45,10 @@ class EmailWhitelistingFilter @Inject()(
     with OtacAuthorisationFunctions
     with Logging {
 
+  implicit val callEq: Eq[Call] = Eq.fromUniversalEquals[Call]
+
+  def toCall(rh: RequestHeader): Call = Call(rh.method, rh.uri)
+
   val whitelistingEnabled: Boolean = config.underlying.getBoolean("passcodeAuthentication.enabled")
 
   val otacUrl: String = config.underlying.getString("otac.url")
@@ -50,13 +58,17 @@ class EmailWhitelistingFilter @Inject()(
   lazy val thereIsAProblemCall: Call =
     uk.gov.hmrc.cgtpropertydisposalsfrontend.controllers.routes.EmailWhitelistingController.thereIsAProblem()
 
-  lazy val excludedPaths: Seq[Call] = Seq(
-    Call("GET", uk.gov.hmrc.play.health.routes.HealthController.ping().url),
-    Call("GET", thereIsAProblemCall.url)
+  lazy val whitelistExclusionRules: Seq[RequestHeader => Boolean] = Seq(
+    toCall(_) === Call("GET", uk.gov.hmrc.play.health.routes.HealthController.ping().url),
+    toCall(_) === Call("GET", thereIsAProblemCall.url),
+    rh => rh.method === "GET" && rh.uri.contains(controllers.template.routes.Template.at("").url),
+    rh => rh.method === "GET" && rh.uri.contains(controllers.routes.Assets.at("").url)
   )
 
+  def excludeRequestFromWhitelist(rh: RequestHeader): Boolean = whitelistExclusionRules.exists(_(rh) === true)
+
   override def apply(f: RequestHeader => Future[Result])(rh: RequestHeader): Future[Result] =
-    if (whitelistingEnabled && !excludedPaths.contains(Call(rh.method, rh.uri))) {
+    if (whitelistingEnabled && !excludeRequestFromWhitelist(rh)) {
       rh.session
         .get(SessionKeys.otacToken)
         .orElse(rh.queryString.get("p").flatMap(_.headOption))
