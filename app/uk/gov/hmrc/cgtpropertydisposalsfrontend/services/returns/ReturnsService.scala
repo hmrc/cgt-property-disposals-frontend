9e2cf3373495a7749308d9d8e961ffc3d79b74e1
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2020-02-26 17:12:48 +0000

    dummy amend return functionality (#215)

--- app/uk/gov/hmrc/cgtpropertydisposalsfrontend/services/returns/ReturnsService.scala
+++ app/uk/gov/hmrc/cgtpropertydisposalsfrontend/services/returns/ReturnsService.scala
@@ -56,6 +56,10 @@ trait ReturnsService {
     implicit hc: HeaderCarrier
   ): EitherT[Future, Error, JsValue]
 
+  def amendReturn(cgtReference: CgtReference, amendedReturn: JsValue)(
+    implicit hc: HeaderCarrier
+  ): EitherT[Future, Error, SubmitReturnResponse]
+
 }
 
 @Singleton
@@ -120,6 +124,17 @@ class ReturnsServiceImpl @Inject() (connector: ReturnsConnector)(implicit ec: Ex
     connector.displayReturn(cgtReference, submissionId).subflatMap { response =>
       if (response.status === OK) {
         response.parseJSON[JsValue]().leftMap(Error(_))
+      } else {
+        Left(Error(s"call to list returns came back with status ${response.status}"))
+      }
+    }
+
+  def amendReturn(cgtReference: CgtReference, amendedReturn: JsValue)(
+    implicit hc: HeaderCarrier
+  ): EitherT[Future, Error, SubmitReturnResponse] =
+    connector.amendReturn(cgtReference, amendedReturn).subflatMap { response =>
+      if (response.status === OK) {
+        response.parseJSON[SubmitReturnResponse]().leftMap(Error(_))
       } else {
         Left(Error(s"call to list returns came back with status ${response.status}"))
       }
