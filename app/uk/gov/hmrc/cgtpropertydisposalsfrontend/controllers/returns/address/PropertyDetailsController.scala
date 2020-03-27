877e1db010d668de903f232089eb7a79f8976b64
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2020-03-27 09:15:49 +0000

    cpdp-1402: acquisition and disposal values pages and cya

--- app/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/returns/address/PropertyDetailsController.scala
+++ app/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/returns/address/PropertyDetailsController.scala
@@ -497,7 +497,7 @@ class PropertyDetailsController @Inject() (
 
             val form = disposalPrice.fold(disposalPriceForm)(c => disposalPriceForm.fill(c.inPounds))
 
-            Ok(multipleDisposalsDisposalPricePage(form, backLink))
+            Ok(multipleDisposalsDisposalPricePage(form, backLink, r.subscribedDetails.isATrust))
         }
     }
   }
@@ -517,7 +517,7 @@ class PropertyDetailsController @Inject() (
               .fold(
                 formWithErrors =>
                   BadRequest(
-                    multipleDisposalsDisposalPricePage(formWithErrors, backLink)
+                    multipleDisposalsDisposalPricePage(formWithErrors, backLink, r.subscribedDetails.isATrust)
                   ), { disposalPrice =>
                   if (answers
                         .fold(_.disposalPrice, c => Some(c.disposalPrice))
@@ -574,7 +574,7 @@ class PropertyDetailsController @Inject() (
 
             val form = acquisitionPrice.fold(acquisitionPriceForm)(c => acquisitionPriceForm.fill(c.inPounds))
 
-            Ok(multipleDisposalsAcquisitionPricePage(form, backLink))
+            Ok(multipleDisposalsAcquisitionPricePage(form, backLink, r.subscribedDetails.isATrust))
         }
     }
   }
@@ -594,7 +594,7 @@ class PropertyDetailsController @Inject() (
               .fold(
                 formWithErrors =>
                   BadRequest(
-                    multipleDisposalsAcquisitionPricePage(formWithErrors, backLink)
+                    multipleDisposalsAcquisitionPricePage(formWithErrors, backLink, r.subscribedDetails.isATrust)
                   ), { acquisitionPrice =>
                   if (answers
                         .fold(_.acquisitionPrice, c => Some(c.acquisitionPrice))
@@ -679,11 +679,18 @@ class PropertyDetailsController @Inject() (
                       logger.warn("Could not update draft return", e)
                       errorHandler.errorResult()
                     },
-                    _ => Ok(multipleDisposalsCheckYourAnswersPage(completeAnswers, hasNonResidentialAssetType))
+                    _ =>
+                      Ok(
+                        multipleDisposalsCheckYourAnswersPage(
+                          completeAnswers,
+                          hasNonResidentialAssetType,
+                          r.subscribedDetails.isATrust
+                        )
+                      )
                   )
 
                 case c: CompleteExamplePropertyDetailsAnswers =>
-                  Ok(multipleDisposalsCheckYourAnswersPage(c, hasNonResidentialAssetType))
+                  Ok(multipleDisposalsCheckYourAnswersPage(c, hasNonResidentialAssetType, r.subscribedDetails.isATrust))
 
               }
 
