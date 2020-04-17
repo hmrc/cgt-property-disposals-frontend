3613f46994a863ba9451eae430bf9c6710e2e12c
sam-miele <9150276+sam-miele@users.noreply.github.com>
2020-04-17 09:18:53 +0100

    Extend test coverage (#433)

---
+++ test/uk/gov/hmrc/cgtpropertydisposalsfrontend/connectors/upscan/UpscanConnectorImplSpec.scala
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cgtpropertydisposalsfrontend.connectors.upscan
+
+import cats.data.EitherT
+import com.typesafe.config.ConfigFactory
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.{Matchers, WordSpec}
+import play.api.libs.json.JsString
+import play.api.mvc.Call
+
+import scala.concurrent.{ExecutionContext, Future}
+import play.api.{Configuration, Mode}
+import play.api.test.Helpers._
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.Generators._
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.connectors.{ConnectorSpec, HttpSupport}
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.upscan.{UploadReference, UpscanInitiateRequest, UpscanUpload}
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+import uk.gov.hmrc.play.bootstrap.config.{RunMode, ServicesConfig}
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.controllers.returns.supportingdocs.routes
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.Error
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class UpscanConnectorImplSpec extends WordSpec with Matchers with MockFactory with HttpSupport {
+
+  val config = Configuration(
+    ConfigFactory.parseString(
+      """
+        | self {
+        |   url = host1.com
+        |  },
+        |  microservice {
+        |    services {
+        |      upscan-initiate {
+        |        protocol = http
+        |        host     = host2
+        |        port     = 123
+        |        user-agent = agent
+        |        max-file-size = 1234
+        |      },
+        |      cgt-property-disposals {
+        |        protocol = http
+        |        host     = host3
+        |        port     = 123
+        |      }
+        |   }
+        |}
+        |""".stripMargin
+    )
+  )
+
+  val connector =
+    new UpscanConnectorImpl(mockHttp, mockWsClient, config, new ServicesConfig(config, new RunMode(config, Mode.Test)))
+
+  "UpscanConnectorImpleSpec" when {
+
+    implicit val hc: HeaderCarrier = HeaderCarrier()
+    val reference                  = sample[UploadReference]
+    val upload                     = sample[UpscanUpload]
+    val baseUrl                    = "http://host3:123/cgt-property-disposals"
+
+    "Inititalising" must {
+      val expectedUrl  = "http://host2:123/upscan/v2/initiate"
+      val mockSuccess  = Call("GET", "/mock-success")
+      val mockFailiure = Call("GET", "/mock-fail")
+
+      val payload = UpscanInitiateRequest(
+        s"$baseUrl/upscan-call-back/upload-reference/${reference.value}",
+        s"host1.com${mockSuccess.url}",
+        s"host1.com${mockFailiure.url}",
+        0,
+        1234
+      )
+      behave like upscanConnectorBehaviour(
+        mockPost[UpscanInitiateRequest](expectedUrl, Map("User-Agent" -> "cgt-property-disposals-frontend"), payload),
+        () => connector.initiate(mockFailiure, mockSuccess, reference)
+      )
+    }
+
+    "getting the upscan upload" must {
+      val expectedUrl = s"$baseUrl/upscan/upload-reference/${reference.value}"
+      behave like upscanConnectorBehaviour(
+        mockGet[HttpResponse](expectedUrl, Map.empty, Map.empty),
+        () => connector.getUpscanUpload(reference)
+      )
+    }
+
+    "saving upscan upload" when {
+      behave like upscanConnectorBehaviour(
+        mockPost[UpscanUpload](s"$baseUrl/upscan", Map.empty, upload),
+        () => connector.saveUpscanUpload(upload)
+      )
+    }
+
+    "update upscan upload" when {
+      val expectedUrl = s"$baseUrl/upscan/upload-reference/${reference.value}"
+      behave like upscanConnectorBehaviour(
+        mockPut[UpscanUpload](expectedUrl, upload),
+        () => connector.updateUpscanUpload(reference, upload)
+      )
+    }
+  }
+
+  def upscanConnectorBehaviour(
+    mockResponse: Option[HttpResponse] => Unit,
+    performCall: () => EitherT[Future, Error, HttpResponse]
+  ) = {
+    "do a get http call and return the result" in {
+      List(
+        HttpResponse(200),
+        HttpResponse(200, Some(JsString("hi")))
+      ).foreach { httpResponse =>
+        withClue(s"For http response [${httpResponse.toString}]") {
+          mockResponse(Some(httpResponse))
+
+          await(performCall().value) shouldBe Right(httpResponse)
+        }
+      }
+    }
+
+    "return an error" when {
+
+      "Internal server error" in {
+        List(
+          HttpResponse(500)
+        ).foreach { httpResponse =>
+          withClue(s"For http response [${httpResponse.toString}]") {
+            mockResponse(Some(httpResponse))
+
+            await(performCall().value).isLeft shouldBe true
+          }
+        }
+      }
+      "the future fails" in {
+        mockResponse(None)
+
+        await(performCall().value).isLeft shouldBe true
+      }
+
+    }
+  }
+}
---
+++ test/uk/gov/hmrc/cgtpropertydisposalsfrontend/services/upscan/UpscanServiceImplSpec.scala
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cgtpropertydisposalsfrontend.services.upscan
+
+import java.util.UUID
+
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+import play.api.mvc.Call
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.connectors.upscan.UpscanConnector
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.upscan.{UploadReference, UploadRequest, UpscanUpload, UpscanUploadMeta}
+
+import cats.data.EitherT
+import cats.instances.future._
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.{Matchers, WordSpec}
+import play.api.libs.json.{JsString, Json}
+import play.api.mvc.Request
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.Generators._
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class UpscanServiceImplSpec extends WordSpec with Matchers with ScalaCheckDrivenPropertyChecks with MockFactory {
+
+  val mockConnector                = mock[UpscanConnector]
+  val service                      = new UpscanServiceImpl(mockConnector)
+  val reference                    = sample[UploadReference]
+  val upload                       = sample[UpscanUpload]
+  implicit val hc: HeaderCarrier   = HeaderCarrier()
+  implicit val request: Request[_] = FakeRequest()
+
+  "UpscanServiceImplSpec" when {
+
+    "hangling requests for upscan" must {
+
+      "getting upscan upload" when {
+        "Response is OK" in {
+          val response = Right(HttpResponse(OK, Some(Json.toJson(upload))))
+          (mockConnector
+            .getUpscanUpload(_: UploadReference)(_: HeaderCarrier))
+            .expects(reference, *)
+            .returning(EitherT.fromEither[Future](response))
+          await(service.getUpscanUpload(reference).value).isRight shouldBe true
+        }
+
+        "Malformed response" in {
+          val response = Right(HttpResponse(OK, Some(JsString("Nope"))))
+          (mockConnector
+            .getUpscanUpload(_: UploadReference)(_: HeaderCarrier))
+            .expects(reference, *)
+            .returning(EitherT.fromEither[Future](response))
+          await(service.getUpscanUpload(reference).value).isLeft shouldBe true
+        }
+
+        "Internal server error" in {
+          val response = Right(HttpResponse(INTERNAL_SERVER_ERROR))
+          (mockConnector
+            .getUpscanUpload(_: UploadReference)(_: HeaderCarrier))
+            .expects(reference, *)
+            .returning(EitherT.fromEither[Future](response))
+          await(service.getUpscanUpload(reference).value).isLeft shouldBe true
+        }
+      }
+
+      "updating upscan" when {
+        "Response is OK" in {
+          val response = Right(HttpResponse(OK))
+          (mockConnector
+            .updateUpscanUpload(_: UploadReference, _: UpscanUpload)(_: HeaderCarrier))
+            .expects(reference, upload, *)
+            .returning(EitherT.fromEither[Future](response))
+          await(service.updateUpscanUpload(reference, upload).value).isRight shouldBe true
+        }
+
+        "Internal server error" in {
+          val response = Right(HttpResponse(INTERNAL_SERVER_ERROR))
+          (mockConnector
+            .updateUpscanUpload(_: UploadReference, _: UpscanUpload)(_: HeaderCarrier))
+            .expects(reference, upload, *)
+            .returning(EitherT.fromEither[Future](response))
+          await(service.updateUpscanUpload(reference, upload).value).isRight shouldBe true
+        }
+      }
+
+      "initialising" when {
+        "initialise" in {
+          val mockSuccess = Call("GET", "/mock-success")
+          val mockFailure = Call("GET", "/mock-fail")
+          val response    = Right(HttpResponse(OK, Some(Json.toJson(UpscanUploadMeta("metadata", sample[UploadRequest])))))
+          (mockConnector
+            .initiate(_: Call, _: Call, _: UploadReference)(_: HeaderCarrier))
+            .expects(mockFailure, mockSuccess, *, *)
+            .returning(EitherT.fromEither[Future](response))
+          (mockConnector
+            .saveUpscanUpload(_: UpscanUpload)(_: HeaderCarrier))
+            .expects(*, *)
+            .returning(EitherT.fromEither[Future](Right(HttpResponse(OK))))
+          await(service.initiate(mockFailure, (_: UploadReference) => mockSuccess).value).isRight shouldBe true
+        }
+      }
+    }
+  }
+}
