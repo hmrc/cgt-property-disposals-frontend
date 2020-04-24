3f87851860db7bc80db5eafcfbbda2ed1bc0a22a
rambabu-posa <rambabu.posa@digital.hmrc.gov.uk>
2020-04-24 15:00:29 +0100

    Revert 'CPDP-1564: Fix for trust logic in confirmation page' (#460)

--- test/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/returns/CheckAllAnswersAndSubmitControllerSpec.scala
+++ test/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/returns/CheckAllAnswersAndSubmitControllerSpec.scala
@@ -622,7 +622,7 @@ class CheckAllAnswersAndSubmitControllerSpec
             "Trust: ",
             noTaxDueRefLine,
             List(submissionLine, addressLine),
-            Left(TrustName("John Doe"))
+            Right(IndividualName("John", "Doe"))
           ),
           TestScenario(
             "organisation representing individual with tax due",
@@ -631,7 +631,7 @@ class CheckAllAnswersAndSubmitControllerSpec
             "Trust: ",
             taxDueRefLine,
             List(submissionLine, returnReferenceWithBundleId, addressLine, taxDueDateLine),
-            Left(TrustName("John Doe"))
+            Right(IndividualName("John", "Doe"))
           )
         )
 
