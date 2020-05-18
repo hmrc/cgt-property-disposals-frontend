f14ffea39df76701ca83d86d7282566913233350
Rambabu Posa <rambabu.posa@digital.hmrc.gov.uk>
2020-05-18 04:38:54 +0100

    wip

--- test/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/returns/TaskListControllerSpec.scala
+++ test/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/returns/TaskListControllerSpec.scala
@@ -14,22 +14,6 @@
  * limitations under the License.
  */
 
-/*
- * Copyright 2020 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package uk.gov.hmrc.cgtpropertydisposalsfrontend.controllers.returns
 
 import java.time.LocalDate
@@ -832,6 +816,7 @@ class TaskListControllerSpec
         }
 
       }
+
     }
 
     "handling requests to display the multiple disposal task list page" must {
@@ -1584,7 +1569,9 @@ class TaskListControllerSpec
         }
 
       }
+
     }
 
   }
+
 }
