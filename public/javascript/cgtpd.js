b5a1afd75893fe896fb92dad5a6d5557458ab602
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2020-05-19 11:56:18 +0100

    CPDP-1467: removed JS file upload error handling

--- public/javascript/cgtpd.js
+++ public/javascript/cgtpd.js
@@ -6,63 +6,6 @@
     errorSummary.focus();
   }
 
-  function handleFileUploadError (input, msg) {
-    $('.file-upload-error').remove();
-    var errorEl = '<span class="error-message file-upload-error" role="alert">' + msg + '</span>';
-    $(errorEl).insertBefore($(input))
-    window.setTimeout(function () {
-      document.querySelector('button[type="submit"]').removeAttribute('disabled')
-    }, 400);
-  }
-
-  function validateFile(file, form, singleFileUpload, submitForm) {
-    if (!file) {
-      var errorMessage = "";
-      if (singleFileUpload.getAttribute("data-is-mandatory-upload") === "true")
-        errorMessage = "Upload a document showing how the Capital Gains Tax due was worked out"
-      else
-        errorMessage = "Upload a file containing your supporting evidence"
-
-      return handleFileUploadError(singleFileUpload, errorMessage);
-    }
-
-    var contentTypes = "application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.oasis.opendocument.spreadsheet,application/pdf,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/vnd.oasis.opendocument.text,text/plain,image/png,image/jpeg";
-
-    if (file.type === "" || contentTypes.indexOf(file.type) === -1) {
-      return handleFileUploadError(singleFileUpload, "The file type " + file.type.toString() + " is not supported.");
-    }
-
-    if (file.size > (3145728)) {
-      return handleFileUploadError(singleFileUpload, "The file must be less than 3MB in size");
-    }
-    if (submitForm) {
-      form.submit();
-    }
-  }
-
-  function validateFileUpload(form, singleFileUpload) {
-    return function (e)  {
-      e.preventDefault();
-      validateFile(e.target.files[0], form, singleFileUpload, false)
-    }
-  }
-
-  function validateFormSubmission(form, singleFileUpload) {
-    return function(e)  {
-      e.preventDefault();
-      var file = singleFileUpload.files[0];
-      validateFile(file, form, singleFileUpload, true);
-    }
-  }
-
-  var singleFileUpload = document.querySelector('input[type="file"]');
-
-  if (singleFileUpload) {
-    var form = document.querySelector('form');
-    singleFileUpload.addEventListener('change', validateFileUpload(form, singleFileUpload));
-    form.addEventListener('submit', validateFormSubmission(form, singleFileUpload))
-  }
-
   var countryEl = document.querySelector("#countryCode");
   if (countryEl) {
     openregisterLocationPicker({
