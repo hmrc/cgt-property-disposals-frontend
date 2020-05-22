11e3c60645d20e477c554e84a12c35afb88625d0
modivilioglu <mo.divilioglu@gmail.com>
2020-05-22 10:56:20 +0100

    [CPDP-1631] - Add tests to discover missing messages (#549)

--- test/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/ControllerSpec.scala
+++ test/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/ControllerSpec.scala
@@ -24,17 +24,62 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
-import play.api.i18n.{Lang, MessagesApi}
+import play.api.http.HttpConfiguration
+import play.api.i18n.{DefaultLangs, DefaultMessagesApi, DefaultMessagesApiProvider, Lang, Langs, MessagesApi}
 import play.api.inject.bind
 import play.api.inject.guice.{GuiceApplicationBuilder, GuiceableModule}
 import play.api.mvc.{Call, Result}
 import play.api.test.Helpers._
-import play.api.{Application, Configuration, Play}
+import play.api.{Application, Configuration, Environment, Logger, Play}
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.config.ViewConfig
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.metrics.{Metrics, MockMetrics}
 
 import scala.concurrent.Future
 import scala.reflect.ClassTag
+import com.google.inject.{Inject, Singleton}
+
+@Singleton
+class TestMessagesApi(
+  messages: Map[String, Map[String, String]],
+  langs: Langs,
+  langCookieName: String,
+  langCookieSecure: Boolean,
+  langCookieHttpOnly: Boolean,
+  httpConfiguration: HttpConfiguration
+) extends DefaultMessagesApi(
+      messages,
+      langs,
+      langCookieName,
+      langCookieSecure,
+      langCookieHttpOnly,
+      httpConfiguration
+    ) {
+
+  override protected def noMatch(key: String, args: Seq[Any])(implicit lang: Lang): String = {
+    Logger.error(s"Could not find message for key: $key ${args.mkString("-")}")
+    s"""not_found_message("$key")"""
+  }
+}
+
+@Singleton
+class TestDefaultMessagesApiProvider @Inject() (
+  environment: Environment,
+  config: Configuration,
+  langs: Langs,
+  httpConfiguration: HttpConfiguration
+) extends DefaultMessagesApiProvider(environment, config, langs, httpConfiguration) {
+
+  override lazy val get: MessagesApi = {
+    new TestMessagesApi(
+      loadAllMessages,
+      langs,
+      langCookieName = langCookieName,
+      langCookieSecure = langCookieSecure,
+      langCookieHttpOnly = langCookieHttpOnly,
+      httpConfiguration = httpConfiguration
+    )
+  }
+}
 
 trait ControllerSpec extends WordSpec with Matchers with BeforeAndAfterAll with MockFactory {
 
@@ -62,6 +107,7 @@ trait ControllerSpec extends WordSpec with Matchers with BeforeAndAfterAll with 
       )
       .disable[uk.gov.hmrc.cgtpropertydisposalsfrontend.repos.SessionStore]
       .overrides(metricsBinding :: overrideBindings: _*)
+      .overrides(bind[MessagesApi].toProvider[TestDefaultMessagesApiProvider])
       .build()
   }
 
@@ -82,10 +128,11 @@ trait ControllerSpec extends WordSpec with Matchers with BeforeAndAfterAll with 
     super.afterAll()
   }
 
-  def messageFromMessageKey(messageKey: String, args: Any*)(implicit
-    messagesApi: MessagesApi
-  ): String =
-    messagesApi(messageKey, args: _*)(lang)
+  def messageFromMessageKey(messageKey: String, args: Any*)(implicit messagesApi: MessagesApi): String = {
+    val m = messagesApi(messageKey, args: _*)(lang)
+    if (m === messageKey) sys.error(s"Message key `$messageKey` is missing a message")
+    m
+  }
 
   def checkIsTechnicalErrorPage(
     result: Future[Result]
@@ -121,6 +168,14 @@ trait ControllerSpec extends WordSpec with Matchers with BeforeAndAfterAll with 
 
     val doc = Jsoup.parse(contentAsString(result))
     doc.select("h1").text should include(expectedTitle)
+
+    val bodyText = doc.select("body").text
+    val regex    = """not_found_message\((.*?)\)""".r
+
+    val regexResult = regex.findAllMatchIn(bodyText).toList
+    if (regexResult.nonEmpty) fail(s"Missing message keys: ${regexResult.map(_.group(1)).mkString(", ")}")
+    else succeed
+
     contentChecks(doc)
   }
 
