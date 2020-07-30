9f1b9bb5ca5d9a53482e32229312b2e721ed768b
Rambabu Posa <rambabu.posa@digital.hmrc.gov.uk>
2020-07-30 09:28:59 +0100

    cya page

--- test/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/returns/yeartodatelliability/YearToDateLiabilityControllerSpec.scala
+++ test/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/returns/yeartodatelliability/YearToDateLiabilityControllerSpec.scala
@@ -3895,6 +3895,7 @@ class YearToDateLiabilityControllerSpec
               messageFromMessageKey("taxableGainOrLoss.personalRepInPeriodOfAdmin.agent.furtherReturn.link")
             )
           }
+
         }
 
       }
