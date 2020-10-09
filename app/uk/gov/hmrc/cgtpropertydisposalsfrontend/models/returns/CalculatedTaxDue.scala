9db1e7bc066e975aa53b3e1c52fdea2c6b18a680
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2020-10-09 13:50:04 +0100

    CPDP-2944: using yearPosition method with AEA

--- app/uk/gov/hmrc/cgtpropertydisposalsfrontend/models/returns/CalculatedTaxDue.scala
+++ app/uk/gov/hmrc/cgtpropertydisposalsfrontend/models/returns/CalculatedTaxDue.scala
@@ -40,10 +40,10 @@ sealed trait CalculatedTaxDue extends Product with Serializable {
   val disposalAmountLessCosts: AmountInPence
   val acquisitionAmountPlusCosts: AmountInPence
   val initialGainOrLoss: AmountInPenceWithSource
+  val gainOrLossAfterInYearLosses: AmountInPence
   val totalReliefs: AmountInPence
   val gainOrLossAfterReliefs: AmountInPence
-  val totalLosses: AmountInPence
-  val gainOrLossAfterLosses: AmountInPence
+  val yearPosition: AmountInPence
   val taxableGainOrNetLoss: AmountInPence
   val amountOfTaxDue: AmountInPence
 }
@@ -54,10 +54,10 @@ object CalculatedTaxDue {
     disposalAmountLessCosts: AmountInPence,
     acquisitionAmountPlusCosts: AmountInPence,
     initialGainOrLoss: AmountInPenceWithSource,
+    gainOrLossAfterInYearLosses: AmountInPence,
     totalReliefs: AmountInPence,
     gainOrLossAfterReliefs: AmountInPence,
-    totalLosses: AmountInPence,
-    gainOrLossAfterLosses: AmountInPence,
+    yearPosition: AmountInPence,
     taxableGainOrNetLoss: AmountInPence,
     amountOfTaxDue: AmountInPence
   ) extends CalculatedTaxDue
@@ -66,10 +66,10 @@ object CalculatedTaxDue {
     disposalAmountLessCosts: AmountInPence,
     acquisitionAmountPlusCosts: AmountInPence,
     initialGainOrLoss: AmountInPenceWithSource,
+    gainOrLossAfterInYearLosses: AmountInPence,
     totalReliefs: AmountInPence,
     gainOrLossAfterReliefs: AmountInPence,
-    totalLosses: AmountInPence,
-    gainOrLossAfterLosses: AmountInPence,
+    yearPosition: AmountInPence,
     taxableGainOrNetLoss: AmountInPence,
     taxableIncome: AmountInPence,
     taxDueAtLowerRate: TaxableAmountOfMoney,
