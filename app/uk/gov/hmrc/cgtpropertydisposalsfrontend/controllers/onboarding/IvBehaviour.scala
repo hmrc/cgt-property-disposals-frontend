dd1c21b440fd6da9bcf1ed5a08f78d170ccc5ddb
ayub96 <75027900+ayub96@users.noreply.github.com>
2022-01-24 22:24:16 +0000

    [DLS-4939]: local redirect fixed

--- app/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/onboarding/IvBehaviour.scala
+++ app/uk/gov/hmrc/cgtpropertydisposalsfrontend/controllers/onboarding/IvBehaviour.scala
@@ -33,6 +33,8 @@ trait IvBehaviour {
 
   private val ivUrl: String = getString("iv.url")
 
+  private val ivEnv: String = getString("iv.env")
+
   private val ivOrigin: String = getString("iv.origin")
 
   private val (ivSuccessUrl: String, ivFailureUrl: String) = {
@@ -48,7 +50,7 @@ trait IvBehaviour {
       (selfBaseUrl + successRelativeUrl) -> (selfBaseUrl + failureRelativeUrl)
   }
 
-  val redirectToIvUrl: String = s"$ivUrl/mdtp/uplift"
+  val redirectToIvUrl: String = s"$ivUrl/$ivEnv/uplift"
 
   val redirectToIv: Result =
     Redirect(
