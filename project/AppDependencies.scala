12474ce9c5b08b776a5a576db4968592f6ff74c1
johno1985 <1640705+johno1985@users.noreply.github.com>
2023-03-24 11:36:58 +0000

    DLS-7230: Bumped bootstrap-frontend library to version 5.25.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "uk.gov.hmrc"                %% "play-frontend-hmrc"               % s"3.5.0-$playVersion",
     "uk.gov.hmrc"                %% "govuk-template"                   % s"5.68.0-$playVersion",
     "uk.gov.hmrc"                %% "play-ui"                          % s"9.4.0-$playVersion",
-    "uk.gov.hmrc"                %% s"bootstrap-frontend-$playVersion" % "5.24.0",
+    "uk.gov.hmrc"                %% s"bootstrap-frontend-$playVersion" % "5.25.0",
     "uk.gov.hmrc.mongo"          %% s"hmrc-mongo-$playVersion"         % "0.70.0",
     "uk.gov.hmrc"                %% "domain"                           % s"6.1.0-$playVersion",
     "org.typelevel"              %% "cats-core"                        % "2.6.0",
