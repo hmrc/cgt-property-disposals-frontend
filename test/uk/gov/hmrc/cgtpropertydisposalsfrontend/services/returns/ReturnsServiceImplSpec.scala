6f2c7c1c40b8cc1aef9ff8051ad623cb32d360bc
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2023-04-14 13:26:40 +0100

    [DLS-7761] used config in spec for maxTaxYears

--- test/uk/gov/hmrc/cgtpropertydisposalsfrontend/services/returns/ReturnsServiceImplSpec.scala
+++ test/uk/gov/hmrc/cgtpropertydisposalsfrontend/services/returns/ReturnsServiceImplSpec.scala
@@ -1875,7 +1875,7 @@ class ReturnsServiceImplSpec extends AnyWordSpec with Matchers with MockFactory 
     "handling requests to list returns" must {
 
       val cgtReference       = sample[CgtReference]
-      val (fromDate, toDate) = LocalDate.now().minusYears(3L).plusDays(1L) -> LocalDate.now()
+      val (fromDate, toDate) = LocalDate.now().minusYears(config.numberOfTaxYearsForReturns).plusDays(1L) -> LocalDate.now()
 
       "return an error " when {
 
