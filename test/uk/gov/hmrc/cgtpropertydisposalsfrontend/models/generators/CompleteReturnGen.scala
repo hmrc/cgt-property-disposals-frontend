c1d250a8312fad31512b3a390bf99ea806331333
Łukasz Wrona <lukasz.andrzej.wrona@outlook.com>
2024-06-11 12:28:44 +0100

    DLS-10424 CompleteReturnGen - cache components

--- test/uk/gov/hmrc/cgtpropertydisposalsfrontend/models/generators/CompleteReturnGen.scala
+++ test/uk/gov/hmrc/cgtpropertydisposalsfrontend/models/generators/CompleteReturnGen.scala
@@ -18,7 +18,18 @@ package uk.gov.hmrc.cgtpropertydisposalsfrontend.models.generators
 
 import org.scalacheck.Gen
 import org.scalacheck.ScalacheckShapeless._
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.returns.AcquisitionDetailsAnswers.CompleteAcquisitionDetailsAnswers
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.returns.CompleteReturn.{CompleteMultipleDisposalsReturn, CompleteMultipleIndirectDisposalReturn, CompleteSingleDisposalReturn, CompleteSingleIndirectDisposalReturn, CompleteSingleMixedUseDisposalReturn}
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.returns.DisposalDetailsAnswers.CompleteDisposalDetailsAnswers
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.returns.ExampleCompanyDetailsAnswers.CompleteExampleCompanyDetailsAnswers
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.returns.ExamplePropertyDetailsAnswers.CompleteExamplePropertyDetailsAnswers
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.returns.ExemptionAndLossesAnswers.CompleteExemptionAndLossesAnswers
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.returns.MixedUsePropertyDetailsAnswers.CompleteMixedUsePropertyDetailsAnswers
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.returns.MultipleDisposalsTriageAnswers.CompleteMultipleDisposalsTriageAnswers
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.returns.RepresenteeAnswers.CompleteRepresenteeAnswers
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.returns.SingleDisposalTriageAnswers.CompleteSingleDisposalTriageAnswers
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.returns.SupportingEvidenceAnswers.CompleteSupportingEvidenceAnswers
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.returns.YearToDateLiabilityAnswers.NonCalculatedYTDAnswers.CompleteNonCalculatedYTDAnswers
 
 object CompleteReturnGen extends LowerPriorityCompleteReturnGen with GenUtils {
 
@@ -28,16 +39,102 @@ object CompleteReturnGen extends LowerPriorityCompleteReturnGen with GenUtils {
 
 trait LowerPriorityCompleteReturnGen { this: GenUtils =>
 
-  implicit val completeMultipleDisposalsReturnGen: Gen[CompleteMultipleDisposalsReturn] =
-    gen[CompleteMultipleDisposalsReturn]
+  private val completeMultipleDisposalsTriageAnswers = gen[CompleteMultipleDisposalsTriageAnswers]
+  private val completeSingleDisposalTriageAnswers    = gen[CompleteSingleDisposalTriageAnswers]
+  private val examplePropertyDetailsAnswers          = gen[CompleteExamplePropertyDetailsAnswers]
+  private val exemptionAndLossesAnswers              = gen[CompleteExemptionAndLossesAnswers]
+  private val yearToDateLiabilityAnswers             = gen[CompleteNonCalculatedYTDAnswers]
+  private val supportingDocumentAnswers              = gen[CompleteSupportingEvidenceAnswers]
+  private val representeeAnswers                     = gen[CompleteRepresenteeAnswers]
+  private val disposalDetails                        = gen[CompleteDisposalDetailsAnswers]
+  private val acquisitionDetails                     = gen[CompleteAcquisitionDetailsAnswers]
 
-  implicit val completeSingleIndirectDisposalReturnGen: Gen[CompleteSingleIndirectDisposalReturn] =
-    gen[CompleteSingleIndirectDisposalReturn]
+  implicit val completeMultipleDisposalsReturnGen: Gen[CompleteMultipleDisposalsReturn] = for {
+    triageAnswers                 <- completeMultipleDisposalsTriageAnswers
+    examplePropertyDetailsAnswers <- examplePropertyDetailsAnswers
+    exemptionAndLossesAnswers     <- exemptionAndLossesAnswers
+    yearToDateLiabilityAnswers    <- yearToDateLiabilityAnswers
+    supportingDocumentAnswers     <- supportingDocumentAnswers
+    representeeAnswers            <- Gen.option(representeeAnswers)
+    gainOrLossAfterReliefs        <- Gen.option(MoneyGen.amountInPenceGen)
+    hasAttachments                <- Generators.booleanGen
+  } yield CompleteMultipleDisposalsReturn(
+    triageAnswers,
+    examplePropertyDetailsAnswers,
+    exemptionAndLossesAnswers,
+    yearToDateLiabilityAnswers,
+    supportingDocumentAnswers,
+    representeeAnswers,
+    gainOrLossAfterReliefs,
+    hasAttachments
+  )
 
-  implicit val completeMultipleIndirectDisposalReturnGen: Gen[CompleteMultipleIndirectDisposalReturn] =
-    gen[CompleteMultipleIndirectDisposalReturn]
+  implicit val completeSingleIndirectDisposalReturnGen: Gen[CompleteSingleIndirectDisposalReturn] = {
+    for {
+      triageAnswers              <- completeSingleDisposalTriageAnswers
+      companyAddress             <- AddressGen.addressGen
+      disposalDetails            <- disposalDetails
+      acquisitionDetails         <- acquisitionDetails
+      exemptionsAndLossesDetails <- exemptionAndLossesAnswers
+      yearToDateLiabilityAnswers <- yearToDateLiabilityAnswers
+      supportingDocumentAnswers  <- supportingDocumentAnswers
+      representeeAnswers         <- Gen.option(representeeAnswers)
+      gainOrLossAfterReliefs     <- Gen.option(MoneyGen.amountInPenceGen)
+      hasAttachments             <- Generators.booleanGen
+    } yield CompleteSingleIndirectDisposalReturn(
+      triageAnswers,
+      companyAddress,
+      disposalDetails,
+      acquisitionDetails,
+      exemptionsAndLossesDetails,
+      yearToDateLiabilityAnswers,
+      supportingDocumentAnswers,
+      representeeAnswers,
+      gainOrLossAfterReliefs,
+      hasAttachments
+    )
+  }
 
-  implicit val completeSingleMixedUseDisposalReturnGen: Gen[CompleteSingleMixedUseDisposalReturn] =
-    gen[CompleteSingleMixedUseDisposalReturn]
+  implicit val completeMultipleIndirectDisposalReturnGen: Gen[CompleteMultipleIndirectDisposalReturn] = for {
+    triageAnswers                <- completeMultipleDisposalsTriageAnswers
+    exampleCompanyDetailsAnswers <- gen[CompleteExampleCompanyDetailsAnswers]
+    exemptionsAndLossesDetails   <- exemptionAndLossesAnswers
+    yearToDateLiabilityAnswers   <- yearToDateLiabilityAnswers
+    supportingDocumentAnswers    <- supportingDocumentAnswers
+    representeeAnswers           <- Gen.option(representeeAnswers)
+    gainOrLossAfterReliefs       <- Gen.option(MoneyGen.amountInPenceGen)
+    hasAttachments               <- Generators.booleanGen
+  } yield CompleteMultipleIndirectDisposalReturn(
+    triageAnswers,
+    exampleCompanyDetailsAnswers,
+    exemptionsAndLossesDetails,
+    yearToDateLiabilityAnswers,
+    supportingDocumentAnswers,
+    representeeAnswers,
+    gainOrLossAfterReliefs,
+    hasAttachments
+  )
+
+  implicit val completeSingleMixedUseDisposalReturnGen: Gen[CompleteSingleMixedUseDisposalReturn] = {
+    for {
+      triageAnswers              <- completeSingleDisposalTriageAnswers
+      propertyDetailsAnswers     <- gen[CompleteMixedUsePropertyDetailsAnswers]
+      exemptionsAndLossesDetails <- exemptionAndLossesAnswers
+      yearToDateLiabilityAnswers <- yearToDateLiabilityAnswers
+      supportingDocumentAnswers  <- supportingDocumentAnswers
+      representeeAnswers         <- Gen.option(representeeAnswers)
+      gainOrLossAfterReliefs     <- Gen.option(MoneyGen.amountInPenceGen)
+      hasAttachments             <- Generators.booleanGen
+    } yield CompleteSingleMixedUseDisposalReturn(
+      triageAnswers,
+      propertyDetailsAnswers,
+      exemptionsAndLossesDetails,
+      yearToDateLiabilityAnswers,
+      supportingDocumentAnswers,
+      representeeAnswers,
+      gainOrLossAfterReliefs,
+      hasAttachments
+    )
+  }
 
 }
