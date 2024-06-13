1605a2874fe0f24dc2e8a9c9be22e1c53dcdcb8d
Łukasz Wrona <lukasz.andrzej.wrona@outlook.com>
2024-06-13 16:53:49 +0100

    DLS-10554 Speed up draftReturnGen

--- test/uk/gov/hmrc/cgtpropertydisposalsfrontend/models/generators/DraftReturnGen.scala
+++ test/uk/gov/hmrc/cgtpropertydisposalsfrontend/models/generators/DraftReturnGen.scala
@@ -16,31 +16,168 @@
 
 package uk.gov.hmrc.cgtpropertydisposalsfrontend.models.generators
 
-import org.scalacheck.Gen
 import org.scalacheck.ScalacheckShapeless._
+import org.scalacheck.{Arbitrary, Gen}
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.returns._
 
 object DraftReturnGen extends HigherPriorityDraftReturnGen with GenUtils
 
-trait HigherPriorityDraftReturnGen extends LowerPriorityDraftReturnGen { this: GenUtils =>
-  implicit val draftReturnGen: Gen[DraftReturn] = gen[DraftReturn]
+trait HigherPriorityDraftReturnGen extends LowerPriorityDraftReturnGen {
+  implicit val singleDisposalDraftReturnGen: Gen[DraftSingleDisposalReturn] = singleDisposalDraftReturnGen2
 
-  implicit val singleDisposalDraftReturnGen: Gen[DraftSingleDisposalReturn] =
-    gen[DraftSingleDisposalReturn]
-
+  implicit val draftReturnGen: Gen[DraftReturn] = Gen.oneOf(
+    singleDisposalDraftReturnGen,
+    singleIndirectDisposalDraftReturnGen,
+    singleMixedUseDraftReturnGen,
+    multipleDisposalDraftReturnGen,
+    multipleIndirectDisposalDraftReturnGen
+  )
 }
 
-trait LowerPriorityDraftReturnGen { this: GenUtils =>
+trait LowerPriorityDraftReturnGen extends GenUtils {
 
-  implicit val multipleDisposalDraftReturnGen: Gen[DraftMultipleDisposalsReturn] = gen[DraftMultipleDisposalsReturn]
+  val triageAnswersGen: Gen[SingleDisposalTriageAnswers]             = gen[SingleDisposalTriageAnswers]
+  private val supportingEvidenceGen: Gen[SupportingEvidenceAnswers]  = gen[SupportingEvidenceAnswers]
+  private val exemptionsAndLossesAnswersGen                          = Gen.oneOf(
+    ExemptionsAndLossesAnswersGen.completeExemptionAndLossesAnswersGen,
+    ExemptionsAndLossesAnswersGen.incompleteExemptionAndLossesAnswersGen
+  )
+  private val disposalDetailsAnswersGen: Gen[DisposalDetailsAnswers] =
+    Gen.oneOf(
+      DisposalDetailsGen.completeDisposalDetailsAnswersGen,
+      DisposalDetailsGen.incompleteDisposalDetailsAnswersGen
+    )
+  private val acquisitionDetailsAnswersGen                           = gen[AcquisitionDetailsAnswers]
 
-  implicit val multipleIndirectDisposalDraftReturnGen: Gen[DraftMultipleIndirectDisposalsReturn] =
-    gen[DraftMultipleIndirectDisposalsReturn]
+  val singleDisposalDraftReturnGen2: Gen[DraftSingleDisposalReturn] =
+    for {
+      id                         <- Gen.uuid
+      triageAnswers              <- triageAnswersGen
+      propertyAddress            <- Gen.option(AddressGen.ukAddressGen)
+      disposalDetailsAnswers     <- Gen.option(disposalDetailsAnswersGen)
+      acquisitionDetailsAnswers  <- Gen.option(acquisitionDetailsAnswersGen)
+      reliefDetailsAnswers       <- Gen.option(gen[ReliefDetailsAnswers])
+      exemptionAndLossesAnswers  <- Gen.option(exemptionsAndLossesAnswersGen)
+      yearToDateLiabilityAnswers <- Gen.option(YearToDateLiabilityAnswersGen.ytdLiabilityAnswersGen)
+      initialGainOrLoss          <- Gen.option(MoneyGen.amountInPenceGen)
+      supportingEvidenceAnswers  <- Gen.option(supportingEvidenceGen)
+      representeeAnswers         <- Gen.option(RepresenteeAnswersGen.representeeAnswersGen)
+      gainOrLossAfterReliefs     <- Gen.option(MoneyGen.amountInPenceGen)
+      lastUpdatedDate            <- Arbitrary.arbitrary[LocalDate]
+    } yield DraftSingleDisposalReturn(
+      id,
+      triageAnswers,
+      propertyAddress,
+      disposalDetailsAnswers,
+      acquisitionDetailsAnswers,
+      reliefDetailsAnswers,
+      exemptionAndLossesAnswers,
+      yearToDateLiabilityAnswers,
+      initialGainOrLoss,
+      supportingEvidenceAnswers,
+      representeeAnswers,
+      gainOrLossAfterReliefs,
+      lastUpdatedDate
+    )
+
+  val multipleTriageGen = gen[MultipleDisposalsTriageAnswers]
+
+  implicit val multipleDisposalDraftReturnGen: Gen[DraftMultipleDisposalsReturn] = {
+    for {
+      id                            <- Gen.uuid
+      triageAnswers                 <- multipleTriageGen
+      examplePropertyDetailsAnswers <- Gen.option(gen[ExamplePropertyDetailsAnswers])
+      exemptionAndLossesAnswers     <- Gen.option(exemptionsAndLossesAnswersGen)
+      yearToDateLiabilityAnswers    <- Gen.option(YearToDateLiabilityAnswersGen.ytdLiabilityAnswersGen)
+      supportingEvidenceAnswers     <- Gen.option(supportingEvidenceGen)
+      representeeAnswers            <- Gen.option(RepresenteeAnswersGen.representeeAnswersGen)
+      gainOrLossAfterReliefs        <- Gen.option(MoneyGen.amountInPenceGen)
+      lastUpdatedDate               <- Arbitrary.arbitrary[LocalDate]
+    } yield DraftMultipleDisposalsReturn(
+      id,
+      triageAnswers,
+      examplePropertyDetailsAnswers,
+      exemptionAndLossesAnswers,
+      yearToDateLiabilityAnswers,
+      supportingEvidenceAnswers,
+      representeeAnswers,
+      gainOrLossAfterReliefs,
+      lastUpdatedDate
+    )
+  }
+
+  implicit val multipleIndirectDisposalDraftReturnGen: Gen[DraftMultipleIndirectDisposalsReturn] = {
+    for {
+      id                           <- Gen.uuid
+      triageAnswers                <- multipleTriageGen
+      exampleCompanyDetailsAnswers <- Gen.option(gen[ExampleCompanyDetailsAnswers])
+      exemptionAndLossesAnswers    <- Gen.option(exemptionsAndLossesAnswersGen)
+      yearToDateLiabilityAnswers   <- Gen.option(YearToDateLiabilityAnswersGen.ytdLiabilityAnswersGen)
+      supportingEvidenceAnswers    <- Gen.option(supportingEvidenceGen)
+      representeeAnswers           <- Gen.option(RepresenteeAnswersGen.representeeAnswersGen)
+      gainOrLossAfterReliefs       <- Gen.option(MoneyGen.amountInPenceGen)
+      lastUpdatedDate              <- Arbitrary.arbitrary[LocalDate]
+    } yield DraftMultipleIndirectDisposalsReturn(
+      id,
+      triageAnswers,
+      exampleCompanyDetailsAnswers,
+      exemptionAndLossesAnswers,
+      yearToDateLiabilityAnswers,
+      supportingEvidenceAnswers,
+      representeeAnswers,
+      gainOrLossAfterReliefs,
+      lastUpdatedDate
+    )
+  }
 
   implicit val singleIndirectDisposalDraftReturnGen: Gen[DraftSingleIndirectDisposalReturn] =
-    gen[DraftSingleIndirectDisposalReturn]
+    for {
+      id                         <- Gen.uuid
+      triageAnswers              <- triageAnswersGen
+      companyAddress             <- Gen.option(AddressGen.addressGen)
+      disposalDetailsAnswers     <- Gen.option(disposalDetailsAnswersGen)
+      acquisitionDetailsAnswers  <- Gen.option(acquisitionDetailsAnswersGen)
+      exemptionAndLossesAnswers  <- Gen.option(exemptionsAndLossesAnswersGen)
+      yearToDateLiabilityAnswers <- Gen.option(YearToDateLiabilityAnswersGen.ytdLiabilityAnswersGen)
+      supportingEvidenceAnswers  <- Gen.option(supportingEvidenceGen)
+      representeeAnswers         <- Gen.option(RepresenteeAnswersGen.representeeAnswersGen)
+      gainOrLossAfterReliefs     <- Gen.option(MoneyGen.amountInPenceGen)
+      lastUpdatedDate            <- Arbitrary.arbitrary[LocalDate]
+    } yield DraftSingleIndirectDisposalReturn(
+      id,
+      triageAnswers,
+      companyAddress,
+      disposalDetailsAnswers,
+      acquisitionDetailsAnswers,
+      exemptionAndLossesAnswers,
+      yearToDateLiabilityAnswers,
+      supportingEvidenceAnswers,
+      representeeAnswers,
+      gainOrLossAfterReliefs,
+      lastUpdatedDate
+    )
 
   implicit val singleMixedUseDraftReturnGen: Gen[DraftSingleMixedUseDisposalReturn] =
-    gen[DraftSingleMixedUseDisposalReturn]
+    for {
+      id                             <- Gen.uuid
+      triageAnswers                  <- triageAnswersGen
+      mixedUsePropertyDetailsAnswers <- Gen.option(gen[MixedUsePropertyDetailsAnswers])
+      exemptionAndLossesAnswers      <- Gen.option(exemptionsAndLossesAnswersGen)
+      yearToDateLiabilityAnswers     <- Gen.option(YearToDateLiabilityAnswersGen.ytdLiabilityAnswersGen)
+      supportingEvidenceAnswers      <- Gen.option(supportingEvidenceGen)
+      representeeAnswers             <- Gen.option(RepresenteeAnswersGen.representeeAnswersGen)
+      gainOrLossAfterReliefs         <- Gen.option(MoneyGen.amountInPenceGen)
+      lastUpdatedDate                <- Arbitrary.arbitrary[LocalDate]
+    } yield DraftSingleMixedUseDisposalReturn(
+      id,
+      triageAnswers,
+      mixedUsePropertyDetailsAnswers,
+      exemptionAndLossesAnswers,
+      yearToDateLiabilityAnswers,
+      supportingEvidenceAnswers,
+      representeeAnswers,
+      gainOrLossAfterReliefs,
+      lastUpdatedDate
+    )
 
 }
