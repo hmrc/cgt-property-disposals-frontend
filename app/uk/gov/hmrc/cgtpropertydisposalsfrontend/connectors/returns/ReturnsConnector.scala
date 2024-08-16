486c9b04670014417c1e08f194a721f7ac7144a5
Łukasz Wrona <lukasz.andrzej.wrona@outlook.com>
2024-08-16 11:28:57 +0100

    DLS-10792 Move JSON parsing of the taxYear function into the connector

--- app/uk/gov/hmrc/cgtpropertydisposalsfrontend/connectors/returns/ReturnsConnector.scala
+++ app/uk/gov/hmrc/cgtpropertydisposalsfrontend/connectors/returns/ReturnsConnector.scala
@@ -26,6 +26,7 @@ import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.Error
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.ids.CgtReference
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.returns._
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.services.returns.ReturnsServiceImpl.{GetDraftReturnResponse, ListReturnsResponse}
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.services.returns.TaxYearServiceImpl.TaxYearResponse
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.util.Logging
 import uk.gov.hmrc.http.HttpReads.Implicits._
 import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpResponse, UpstreamErrorResponse}
@@ -75,9 +76,7 @@ trait ReturnsConnector {
     request: YearToDateLiabilityCalculationRequest
   )(implicit hc: HeaderCarrier): EitherT[Future, Error, YearToDateLiabilityCalculation]
 
-  def taxYear(date: LocalDate)(implicit
-    hc: HeaderCarrier
-  ): EitherT[Future, Error, HttpResponse]
+  def taxYear(date: LocalDate)(implicit hc: HeaderCarrier): EitherT[Future, Error, TaxYearResponse]
 
   def availableTaxYears()(implicit hc: HeaderCarrier): EitherT[Future, Error, HttpResponse]
 
@@ -190,15 +189,10 @@ class ReturnsConnectorImpl @Inject() (http: HttpClient, servicesConfig: Services
       request
     ) pipe handleErrors(s"POST to $calculateYearToDateLiabilityUrl")
 
-  def taxYear(
-    date: LocalDate
-  )(implicit hc: HeaderCarrier): EitherT[Future, Error, HttpResponse] =
-    EitherT[Future, Error, HttpResponse](
-      http
-        .GET[HttpResponse](s"$baseUrl/tax-year/${date.format(dateFormatter)}")
-        .map(Right(_))
-        .recover { case e => Left(Error(e)) }
-    )
+  def taxYear(date: LocalDate)(implicit hc: HeaderCarrier): EitherT[Future, Error, TaxYearResponse] = {
+    val url = s"$baseUrl/tax-year/${date.format(dateFormatter)}"
+    http.GET[Either[UpstreamErrorResponse, TaxYearResponse]](url) pipe handleErrors(s"GET to $url")
+  }
 
   def availableTaxYears()(implicit hc: HeaderCarrier): EitherT[Future, Error, HttpResponse] =
     EitherT[Future, Error, HttpResponse](
