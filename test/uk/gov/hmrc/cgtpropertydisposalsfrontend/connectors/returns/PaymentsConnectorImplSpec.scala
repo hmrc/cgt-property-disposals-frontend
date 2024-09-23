007d8eadc075aa7da9e555b0b7c618d55f90dbb7
gajalakshmiseetharaman <177637840+gajalakshmiseetharaman@users.noreply.github.com>
2024-09-23 12:33:52 +0100

    DLS-10794 wiremock update

--- test/uk/gov/hmrc/cgtpropertydisposalsfrontend/connectors/returns/PaymentsConnectorImplSpec.scala
+++ test/uk/gov/hmrc/cgtpropertydisposalsfrontend/connectors/returns/PaymentsConnectorImplSpec.scala
@@ -18,24 +18,36 @@ package uk.gov.hmrc.cgtpropertydisposalsfrontend.connectors.returns
 
 import com.typesafe.config.ConfigFactory
 import org.scalamock.scalatest.MockFactory
+import org.scalatest.EitherValues
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
-import play.api.Configuration
-import play.api.libs.json.Json
-import uk.gov.hmrc.cgtpropertydisposalsfrontend.connectors.{ConnectorSpec, HttpSupport}
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.{Application, Configuration}
+import play.api.libs.json.{JsString, Json}
+import play.api.test.Helpers.{await, defaultAwaitTimeout}
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.controllers
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.finance.AmountInPence
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.generators.Generators._
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.generators.IdGen._
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.generators.MoneyGen._
 import uk.gov.hmrc.cgtpropertydisposalsfrontend.models.ids.CgtReference
-import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.cgtpropertydisposalsfrontend.util.WireMockMethods
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpResponse}
+import uk.gov.hmrc.http.test.WireMockSupport
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
 import java.time.LocalDate
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.ExecutionContext
 
-class PaymentsConnectorImplSpec extends AnyWordSpec with Matchers with MockFactory with HttpSupport with ConnectorSpec {
+class PaymentsConnectorImplSpec
+    extends AnyWordSpec
+    with Matchers
+    with MockFactory
+    with GuiceOneAppPerSuite
+    with WireMockSupport
+    with WireMockMethods
+    with EitherValues {
 
   val selfUrl = "https://self:999"
 
@@ -45,9 +57,9 @@ class PaymentsConnectorImplSpec extends AnyWordSpec with Matchers with MockFacto
         |microservice {
         |  services {
         |    payments {
-        |      protocol = https
-        |      host     = host
-        |      port     = 123
+        |      protocol = http
+        |      host     = $wireMockHost
+        |      port     = $wireMockPort
         |    }
         |  }
         |}
@@ -57,15 +69,16 @@ class PaymentsConnectorImplSpec extends AnyWordSpec with Matchers with MockFacto
     )
   )
 
+  override def fakeApplication(): Application = new GuiceApplicationBuilder().configure(config).build()
+  val mockHttp: HttpClient                    = app.injector.instanceOf[HttpClient]
+  implicit lazy val ec: ExecutionContext = app.injector.instanceOf[ExecutionContext]
   val connector = new PaymentsConnectorImpl(
     mockHttp,
     new ServicesConfig(config)
   )
-
   implicit val hc: HeaderCarrier = HeaderCarrier()
 
   "PaymentsConnectorImpl" when {
-
     "handling requests to start a payments journey" must {
       val cgtReference    = sample[CgtReference]
       val chargeReference = sample[String]
@@ -73,39 +86,87 @@ class PaymentsConnectorImplSpec extends AnyWordSpec with Matchers with MockFacto
       val dueDate         = LocalDate.parse("2023-04-05")
       val returnCall      = controllers.routes.StartController.start()
       val backCall        = controllers.returns.routes.TaskListController.taskList()
-      val expectedUrl     =
-        "https://host:123/pay-api/capital-gains-tax/journey/start"
+      val expectedUrl     = "/pay-api/capital-gains-tax/journey/start"
+      "do a get http call and return the result" in {
+        List(
+          HttpResponse(200, "{}"),
+          HttpResponse(200, JsString("hi"), Map.empty[String, Seq[String]]),
+          HttpResponse(500, "{}")
+        ).foreach { httpResponse =>
+          withClue(s"For http response [${httpResponse.toString}]") {
+            when(
+              POST,
+              expectedUrl,
+              body = Some(
+                Json
+                  .parse(
+                    s"""
+                   |{
+                   |  "cgtReference": "${cgtReference.value}",
+                   |  "chargeReference": "$chargeReference",
+                   |  "amountInPence": ${amount.value},
+                   |  "dueDate": "2023-04-05",
+                   |  "returnUrl": "$selfUrl${returnCall.url}",
+                   |  "backUrl": "$selfUrl${backCall.url}"
+                   |}
+                   |""".stripMargin
+                  )
+                  .toString()
+              )
+            ).thenReturn(
+              httpResponse.status,
+              httpResponse.body
+            )
+            val result = await(
+              connector
+                .startPaymentJourney(cgtReference, Some(chargeReference), amount, Some(dueDate), returnCall, backCall)
+                .value
+            ).value
 
-      behave like connectorBehaviour(
-        mockPost(
-          expectedUrl,
-          Seq.empty,
-          Json.parse(
-            s"""
-              |{
-              |  "cgtReference": "${cgtReference.value}",
-              |  "chargeReference": "$chargeReference",
-              |  "amountInPence": ${amount.value},
-              |  "dueDate": "2023-04-05",
-              |  "returnUrl": "$selfUrl${returnCall.url}",
-              |  "backUrl": "$selfUrl${backCall.url}"
-              |}
-              |""".stripMargin
-          )
-        ),
-        () =>
-          connector.startPaymentJourney(
-            cgtReference,
-            Some(chargeReference),
-            amount,
-            Some(dueDate),
-            returnCall,
-            backCall
-          )
-      )
+            result.status shouldBe httpResponse.status
+            result.body   shouldBe httpResponse.body
+          }
+        }
+      }
 
+      "return an error" when {
+        "the future fails" in {
+          wireMockServer.stop()
+          when(
+            POST,
+            expectedUrl,
+            body = Some(
+              Json
+                .parse(
+                  s"""
+                 |{
+                 |  "cgtReference": "${cgtReference.value}",
+                 |  "chargeReference": "$chargeReference",
+                 |  "amountInPence": ${amount.value},
+                 |  "dueDate": "2023-04-05",
+                 |  "returnUrl": "$selfUrl${returnCall.url}",
+                 |  "backUrl": "$selfUrl${backCall.url}"
+                 |}
+                 |""".stripMargin
+                )
+                .toString()
+            )
+          )
+
+          await(
+            connector
+              .startPaymentJourney(
+                cgtReference,
+                Some(chargeReference),
+                amount,
+                Some(dueDate),
+                returnCall,
+                backCall)
+              .value
+          ).isLeft shouldBe true
+          wireMockServer.start()
+        }
+      }
     }
-
   }
-
 }
