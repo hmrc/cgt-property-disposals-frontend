7cae669e575b61df15e345021cd3c4a8b90dd861
gajalakshmiseetharaman <177637840+gajalakshmiseetharaman@users.noreply.github.com>
2025-09-25 11:50:44 +0100

    DLS-12182: Gov UK rebranding

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,24 +2,24 @@ import sbt.*
 
 object AppDependencies {
   val playVersion      = "play-30"
-  val bootstrapVersion = "9.13.0"
+  val bootstrapVersion = "10.1.0"
   val mongoVersion     = "2.7.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion" % "12.8.0",
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion" % "12.14.0",
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"         % mongoVersion,
-    "uk.gov.hmrc"       %% s"domain-$playVersion"             % "12.1.0",
+    "uk.gov.hmrc"       %% s"domain-$playVersion"             % "13.0.0",
     "org.typelevel"     %% "cats-core"                        % "2.13.0",
     "dev.optics"        %% "monocle-core"                     % "3.3.0",
     "dev.optics"        %% "monocle-macro"                    % "3.3.0"
   )
 
   def test(scope: String = "test"): Seq[ModuleID] = Seq(
-    "org.jsoup"           % "jsoup"                         % "1.21.1"         % scope,
-    "org.scalamock"      %% "scalamock"                     % "7.4.0"          % scope,
+    "org.jsoup"           % "jsoup"                         % "1.21.2"         % scope,
+    "org.scalamock"      %% "scalamock"                     % "7.5.0"          % scope,
     "org.scalatest"      %% "scalatest"                     % "3.2.19"         % scope,
-    "org.scalacheck"     %% "scalacheck"                    % "1.18.1"         % scope,
+    "org.scalacheck"     %% "scalacheck"                    % "1.19.0"         % scope,
     "io.github.martinhh" %% "scalacheck-derived"            % "0.10.0"          % scope,
     "org.scalatestplus"   % "scalacheck-1-18_3"             % "3.2.19.0"       % scope,
     "uk.gov.hmrc.mongo"  %% s"hmrc-mongo-test-$playVersion" % mongoVersion     % scope,
