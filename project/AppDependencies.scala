eea56bafbf3115a4457650ec454111a50a171003
ruthdas03 <86231805+ruthdas03@users.noreply.github.com>
2026-02-26 09:48:16 +0000

    [DLS-12760]Upgrade bootstrap-play to 10.6.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
   val playVersion      = "play-30"
-  val bootstrapVersion = "10.5.0"
+  val bootstrapVersion = "10.6.0"
   val mongoVersion     = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
